b98e09131f1c2838bb1544b902d8e41879a907ac
cash <cash@cashwu.com>
2017-08-28 21:22:26 +0800

    inpt good 1,1,1 and evil 1,1,1 test case

--- Kata20170828_GoodvsEvil/GoodvsEvilTests.cs
+++ Kata20170828_GoodvsEvil/GoodvsEvilTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170828_GoodvsEvil
@@ -12,10 +13,22 @@ namespace Kata20170828_GoodvsEvil
             GoodvsEvilShouldBeNoVictor("1 0 0 0 0 0", "1 0 0 0 0 0 0");
         }
 
+        [TestMethod]
+        public void input_good_1_1_1_and_evil_1_1_1_should_return_good_triumph()
+        {
+            GoodvsEvilShouldBeGoodTriumph("1 1 1 0 0 0", "1 1 1 0 0 0 0");
+        }
+
         private static void GoodvsEvilShouldBeNoVictor(string good, string evil)
         {
             var actual = new Kata().GoodVsEvil(good, evil);
             Assert.AreEqual("Battle Result: No victor on this battle field", actual);
+        }
+
+        private static void GoodvsEvilShouldBeGoodTriumph(string good, string evil)
+        {
+            var actual = new Kata().GoodVsEvil(good, evil);
+            Assert.AreEqual("Battle Result: Good triumphs over Evil", actual);
         }
     }
 
@@ -23,6 +36,17 @@ namespace Kata20170828_GoodvsEvil
     {
         public string GoodVsEvil(string good, string evil)
         {
+            var goodPeopleWorth = new[] { 1, 2, 3, 3, 4, 10 };
+            var evilPeopleWorth = new[] { 1, 2, 2, 2, 3, 5, 10 };
+
+            var goodWorth = good.Split(' ').Select((a, i) => goodPeopleWorth[i] * int.Parse(a)).Sum();
+            var evilWorth = evil.Split(' ').Select((a, i) => evilPeopleWorth[i] * int.Parse(a)).Sum();
+
+            if (goodWorth > evilWorth)
+            {
+                return "Battle Result: Good triumphs over Evil";
+            }
+
             return "Battle Result: No victor on this battle field";
         }
     }
