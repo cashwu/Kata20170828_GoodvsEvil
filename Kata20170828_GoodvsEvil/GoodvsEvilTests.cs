92a5908310ec337e1d72b17d8012529ab45da3d1
cash <cash@cashwu.com>
2017-08-28 21:13:45 +0800

    input good 1, evil 1 test case

--- Kata20170828_GoodvsEvil/GoodvsEvilTests.cs
+++ Kata20170828_GoodvsEvil/GoodvsEvilTests.cs
@@ -7,8 +7,23 @@ namespace Kata20170828_GoodvsEvil
     public class GoodvsEvilTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_good_1_and_evil_1_should_return_no_victor()
         {
+            GoodvsEvilShouldBeNoVictor("1 0 0 0 0 0", "1 0 0 0 0 0 0");
+        }
+
+        private static void GoodvsEvilShouldBeNoVictor(string good, string evil)
+        {
+            var actual = new Kata().GoodVsEvil(good, evil);
+            Assert.AreEqual("Battle Result: No victor on this battle field", actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string GoodVsEvil(string good, string evil)
+        {
+            return "Battle Result: No victor on this battle field";
         }
     }
 }
